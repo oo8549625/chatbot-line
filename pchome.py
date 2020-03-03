52dc740d7734189215c6955361c05717b574cdc9
YE,LI-WEI <a0970366492@gmail.com>
2020-03-03 13:41:59 +0800

    debug

--- pchome.py
+++ pchome.py
@@ -8,7 +8,8 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-
+driver = webdriver.Chrome(executable_path=os.environ.get(
+    "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 # Now you can start using Selenium
 
 
@@ -45,7 +46,7 @@ def scrapy(search_id):
             price = prod['price']
 
             print("搜尋:" + id)
-            with webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options) as driver:
+            with driver:
                 driver.implicitly_wait(10)
                 driver.get("https://mall.pchome.com.tw/prod/" + id)  # 前往這個網址
                 print("網頁資源:" + driver.page_source)
