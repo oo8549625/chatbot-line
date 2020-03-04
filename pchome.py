21e46c860b21b026b7523031a7e317f13572e4fa
oo8549625 <a0970366492@gmail.com>
2020-03-05 02:18:27 +0800

    wait debug

--- pchome.py
+++ pchome.py
@@ -56,10 +56,9 @@ def scrapy(search_id):
             price = prod['price']
 
             print("搜尋:" + id)
-            driver.implicitly_wait(5)
+            driver.implicitly_wait(1)
             driver.get("https://mall.pchome.com.tw/prod/" + id)
-            # 前往這個網址
-            # print("網頁資源:" + driver.page_source)
+
             gifts = []
             for data in driver.find_elements_by_css_selector("a[class='giftlink']"):
                 gifts.append(data.text)
